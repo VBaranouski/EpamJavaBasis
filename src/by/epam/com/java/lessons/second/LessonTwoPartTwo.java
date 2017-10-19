99a651226307339235d019f47c6570db05f969e0
Vlad <vladislav.baranovski@gmail.com>
2017-10-19 23:00:25 +0300

    updates in the Lesson 2 part 2

--- src/by/epam/com/java/lessons/second/LessonTwoPartTwo.java
+++ src/by/epam/com/java/lessons/second/LessonTwoPartTwo.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class LessonTwoPartTwo {
 
-    public static void daysChecker() {
+    public static void dayCountChecker() {
 
         Scanner sc = new Scanner(System.in);
 
@@ -81,6 +81,47 @@ public class LessonTwoPartTwo {
         }
 
         return numDays;
+    }
+
+    public static void seasonChecker() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите номер месяца: ");
+        Integer monthNumber = Integer.valueOf(sc.nextLine());
+        seasonDefiner(monthNumber);
+
+    }
+
+    private static void seasonDefiner(Integer monthNumber) {
+        switch (monthNumber) {
+        case 12:
+        case 1:
+        case 2:
+            System.out.println("Зима");
+            break;
+
+        case 3:
+        case 4:
+        case 5:
+            System.out.println("Весна");
+            break;
+
+        case 6:
+        case 7:
+        case 8:
+            System.out.println("Лето");
+            break;
+
+        case 9:
+        case 10:
+        case 11:
+            System.out.println("Осень");
+            break;
+
+        default:
+            System.out.println("Несуществующий месяц");
+            break;
+        }
+
     }
 
     //<---- Arrays ---->
