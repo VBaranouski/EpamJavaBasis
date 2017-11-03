327af1beeace551727e407f4afb72e5531ef6885
Vlad <vladislav.baranovski@gmail.com>
2017-11-03 12:34:22 +0300

    Lesson 4 part 1 & updates in previous tasks

--- src/by/epam/com/java/Runner.java
+++ src/by/epam/com/java/Runner.java
@@ -9,10 +9,10 @@ import by.epam.com.java.lessons.third.LessonThreePartOne;
 
 public class Runner {
 
-    static LessonOnePartOne fLPartOne;
-    static LessonOnePartTwo fLPartTwo;
-    static LessonTwoPartOne lTPartOne;
-    static LessonTwoPartTwo lTPartTwo;
+    static LessonOnePartOne lFirstPartOne;
+    static LessonOnePartTwo lFirstPartTwo;
+    static LessonTwoPartOne lTwoPartOne;
+    static LessonTwoPartTwo lTwoPartTwo;
     static LessonThreePartOne lThreePartOne;
 
     public static void main(String[] args) {
@@ -25,36 +25,38 @@ public class Runner {
         System.out.println("Sum of arguments: " + sum);
 
         // <---- Lesson one part one ---->
-
-        fLPartOne.showHelloWorld();   // 1 Hello World!
-        fLPartOne.showPersonalInfo();    // 2 Выведите на консоль ваши ФИО, адрес и телефон.
-        fLPartOne.showBookText();   // 3 Выведите на экран текст.
-        fLPartOne.calculateHypotenuse(3, 4);   // 4 Гипотенуза по двум катетам
-        fLPartOne.squareOfRightTriangle(5, 4);   // 4 Площадь по двум катетам
-        fLPartOne.calculateSumOfFourth(1, 2, 3, 4);   // 5 Вычисленить суммы четырёх слагаемых.
-        fLPartOne.calculateExpression();    // 6 Вычислить значение выражения a2-(b-c)2+ln(b2+1).
-        fLPartOne.newYear(4, 4, 5);  // 7 «Новый год!».
+        lFirstPartOne.showHelloWorld();   // 1 Hello World!
+        lFirstPartOne.showPersonalInfo();    // 2 Выведите на консоль ваши ФИО, адрес и телефон.
+        lFirstPartOne.showBookText();   // 3 Выведите на экран текст.
+        lFirstPartOne.calculateHypotenuse(3, 4);   // 4 Гипотенуза по двум катетам
+        lFirstPartOne.squareOfRightTriangle(5, 4);   // 4 Площадь по двум катетам
+        lFirstPartOne.calculateSumOfFourth(1, 2, 3, 4);   // 5 Вычисленить суммы четырёх слагаемых.
+        lFirstPartOne.calculateExpression();    // 6 Вычислить значение выражения a2-(b-c)2+ln(b2+1).
+        lFirstPartOne.newYear(4, 4, 5);  // 7 «Новый год!».
 
         // <---- Lesson one part two ---->
 
-        fLPartTwo.intOperations();   // 2 Программа запрашивает количество чисел, затем вводит целые числа..
-        fLPartTwo.generatorOfNDoubles(10);   // 3 Программа вводит количество N вещественных чисел...
-        fLPartTwo.generatorOfNIntegers(7);   // 4 Программа запрашивает количество чисел, вводит целые числа...
-        fLPartTwo.calculateExpressions(12, 8, -3.5, 0.2);   // 6 Вычислить значение выражения по формуле../
-        fLPartTwo.operationsWithNumbers(10, -2, 3);  // 7 Даны три действительных числа. Возвести в квадрат...
-        fLPartTwo.gettingFunctionValues(2.1, 9, 1.2);  // 8 Составить программу для вычисления значений функции...
-        fLPartTwo.calculateCircle(10.2);   // 9 Вычислить длину окружности и площадь...
-        fLPartTwo.calculateFunction(2);    // 10 Вычислить щначение функции...
+        lFirstPartTwo.intOperations();   // 2 Программа запрашивает количество чисел, затем вводит целые числа..
+        lFirstPartTwo.generatorOfNDoubles(10);   // 3 Программа вводит количество N вещественных чисел...
+        lFirstPartTwo.generatorOfNIntegers(7);   // 4 Программа запрашивает количество чисел, вводит целые числа...
+        lFirstPartTwo.calculateExpressions(12, 8, -3.5, 0.2);   // 6 Вычислить значение выражения по формуле../
+        lFirstPartTwo.operationsWithNumbers(10, -2, 3);  // 7 Даны три действительных числа. Возвести в квадрат...
+        lFirstPartTwo.gettingFunctionValues(2.1, 9, 1.2);  // 8 Составить программу для вычисления значений функции...
+        lFirstPartTwo.calculateCircle(10.2);   // 9 Вычислить длину окружности и площадь...
+        lFirstPartTwo.calculateFunction(2);    // 10 Вычислить щначение функции...
 
         // <---- Lesson two part one ---->
 
-        lTPartOne.sumAndCompUsingMethods();  //Решите задачу, находящую сумму и произведение двух чисел с использованием методов..
+        lTwoPartOne
+                .sumAndCompUsingMethods();  //Решите задачу, находящую сумму и произведение двух чисел с использованием методов..
 
         // <---- Lesson two part two ---->
 
-        lTPartTwo.dayCountChecker();  // 2. Перепишите код из листинка l2-2 используя методы. 4. Перепишите код с помощью операторов if.
-        lTPartTwo.seasonChecker();  // 3. Введите номер месяца как строку, с помощью valueOf переведите ее в элемент перечисления
-        lTPartTwo.arrayCreator(5); // 7. Создайте одномерный массив вещественных чисел. Min/Max/Sorting
+        lTwoPartTwo
+                .dayCountChecker();  // 2. Перепишите код из листинка l2-2 используя методы. 4. Перепишите код с помощью операторов if.
+        lTwoPartTwo
+                .seasonChecker();  // 3. Введите номер месяца как строку, с помощью valueOf переведите ее в элемент перечисления
+        lTwoPartTwo.arrayCreator(5); // 7. Создайте одномерный массив вещественных чисел. Min/Max/Sorting
 
         // <---- Lesson three part one ---->
 
@@ -67,20 +69,22 @@ public class Runner {
         lThreePartOne.arrayFromEvenNumbers(
                 10); //4 Дана последовательность натуральных чисел а1 , а2 ,..., ап. Создать массив из четных чисел этой последовательности. Если таких чисел нет, то вывести сообщение об этом факте.
 
+        // <---- Lesson three part two ---->
 
-         // <---- Lesson three part two ---->
+        Fraction fraction = new Fraction();  //1. Определить класс «Дробь» в виде пары m/n. Объявить массив из k дробей, ввести/вывести значения для массива дробей.
+        fraction.createFractionArray(10);  // Note: Дроби заполняются через Random
 
-         Fraction fraction = new Fraction();  //1. Определить класс «Дробь» в виде пары m/n. Объявить массив из k дробей, ввести/вывести значения для массива дробей.
-         fraction.createFractionArray(10);  // Note: Дроби заполняются через Random
+        Group group = new Group(); // 2. Определить: а) средний балл учебной группы (использовать класс Group); ...
+        group.createStudentsGroup(); // .. б) средний балл каждого студента; в) число отличников; ...
+        group.calculateBadAndGoodStudents();  // ... количество студентов, имеющих "2"
 
-         Group group = new Group(); // 2. Определить: а) средний балл учебной группы (использовать класс Group); ...
-         group.createStudentsGroup(); // .. б) средний балл каждого студента; в) число отличников; ...
-         group.calculateBadAndGoodStudents();  // ... количество студентов, имеющих "2"
-
-*/
         // <---- Lesson four part one ---->
-       // LessonFourPartOne.twoDimensionalFibonachiArray(5,5);
-        LessonFourPartOne.sortTwoDimensionalArrayByColumn();
+
+        LessonFourPartOne.twoDimensionalFibonachiArray(5,
+                5); // 1. Инициализировать массив, в котором следующюй элемент равер сумме двух предыдущих
+     */
+        LessonFourPartOne
+                .sortTwoDimensionalArrayByColumn(); // 2. Создайте массив массивов с одинаковым количеством элментов в каждой строке. Отсортируйте элементы этого массива ‘по столбикам’.
 
     }
 
