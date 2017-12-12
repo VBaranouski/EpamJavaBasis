602b23f0d0b9149c7acc48772a258d3d07f86c5a
Vlad <vladislav.baranovski@gmail.com>
2017-12-12 23:50:14 +0300

    Lesson 6 parts 1 & 2. FileReader and parsing

--- src/by/epam/com/java/lessons/six_and_seven/Library.java
+++ src/by/epam/com/java/lessons/six_and_seven/Library.java
@@ -7,24 +7,16 @@ public class Library {
 
     public static void main(String[] args) throws IOException {
 
-        Book foundBook;
         LibraryActions book = new LibraryActions();
         book.add(new Book("The Shining", "Stephen King", 1977, 25.3));
         book.add(new Book("It", "Stephen King", 1987, 13.99));
         book.add(new Book("The Dark Tower VII", "Stephen King", 2005, 17.3));
 
-        foundBook = book.findBookByTitle("It");   //find book by title
-        if (foundBook == null) {
-            System.out.println("\n" + "No such book found");
-        }
+        Book foundBook = book.findBookByTitle("It");   //find book by title
         book.printBook(foundBook);
 
-        List<Book> foundBooks = book.findBookByAuthor("Stephen Kingsss");
-        if (foundBooks != null) {
-            book.printBook(foundBooks);
-
-        }
-        System.out.println("\n" + "No such book found");
+        List<Book> foundBooks = book.findBookByAuthor("Stephen King");
+        book.printBook(foundBooks);
 
 
         BookReader reader = new BookReader();
--- src/by/epam/com/java/lessons/six_and_seven/LibraryActions.java
+++ src/by/epam/com/java/lessons/six_and_seven/LibraryActions.java
@@ -43,15 +43,22 @@ public class LibraryActions {
     }
 
     public void printBook(Book book) {
-        System.out.println("\n" + "Found book: ");
+        if (book != null) {
+        System.out.println("\n" + "Found book by title: ");
         System.out.println(book.getTitle() + ", " + book.getAuthor() + ", " + book.getYear());
-
+    } else {
+            System.out.println("\n" + "No books with such title found");
+        }
     }
 
     public void printBook(List<Book> foundBooks) {
-        System.out.println("\n" + "Found books: ");
-        for (Book bk : foundBooks) {
-            System.out.println(bk.getTitle() + ", " + bk.getAuthor() + ", " + bk.getYear());
+        if (foundBooks != null) {
+            System.out.println("\n" + "Found books by author: ");
+            for (Book bk : foundBooks) {
+                System.out.println(bk.getAuthor() + ", " + bk.getTitle() + ", " + bk.getYear());
+            }
+        }else {
+            System.out.println("\n" + "No books with such author found");
         }
 
     }
