4bb145b75e49722edb7e81d2ecaac811eb979ceb
Vlad <vladislav.baranovski@gmail.com>
2017-12-13 18:44:36 +0300

    Lesson 6 parts 1 & 2. FileReader and parsing

--- src/by/epam/com/java/lessons/six_and_seven/Library.java
+++ src/by/epam/com/java/lessons/six_and_seven/Library.java
@@ -11,13 +11,13 @@ public class Library {
         book.add(new Book("The Shining", "Stephen King", 1977, 25.3));
         book.add(new Book("It", "Stephen King", 1987, 13.99));
         book.add(new Book("The Dark Tower VII", "Stephen King", 2005, 17.3));
+        book.printAllBooks(book);
 
         Book foundBook = book.findBookByTitle("It");   //find book by title
         book.printBook(foundBook);
 
         List<Book> foundBooks = book.findBookByAuthor("Stephen King");
-        book.printBook(foundBooks);
-
+        book.printBook(foundBooks, true);
 
         BookReader reader = new BookReader();
         reader.readAndPrint(reader);
--- src/by/epam/com/java/lessons/six_and_seven/LibraryActions.java
+++ src/by/epam/com/java/lessons/six_and_seven/LibraryActions.java
@@ -1,6 +1,8 @@
 package by.epam.com.java.lessons.six_and_seven;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class LibraryActions {
@@ -10,7 +12,6 @@ public class LibraryActions {
     public void add(Book book) {
         books.add(book);
     }
-
 
     public Book findBookByTitle(String title) {
         for (Book book : books) {
@@ -38,28 +39,49 @@ public class LibraryActions {
         System.out.println("\n" + "Library collection: ");
         List<Book> showBooks = book.getBooks();
         for (Book bk : showBooks) {
-            printBook(bk);
+            System.out.println(bk.getAuthor() + ", " + bk.getTitle() + ", " + bk.getYear());
         }
     }
 
     public void printBook(Book book) {
         if (book != null) {
-        System.out.println("\n" + "Found book by title: ");
-        System.out.println(book.getTitle() + ", " + book.getAuthor() + ", " + book.getYear());
-    } else {
+            System.out.println("\n" + "Found book by title: ");
+            System.out.println(book.getTitle() + ", " + book.getAuthor() + ", " + book.getYear());
+        } else {
             System.out.println("\n" + "No books with such title found");
         }
     }
 
-    public void printBook(List<Book> foundBooks) {
+    public void printBook(List<Book> foundBooks, boolean shouldBeSortedByYear) {
         if (foundBooks != null) {
+            if (shouldBeSortedByYear) {
+                sortBooksByYear(foundBooks);
+            }
             System.out.println("\n" + "Found books by author: ");
             for (Book bk : foundBooks) {
                 System.out.println(bk.getAuthor() + ", " + bk.getTitle() + ", " + bk.getYear());
             }
-        }else {
+        } else {
             System.out.println("\n" + "No books with such author found");
         }
+
+    }
+
+    public static void sortBooksByYear(List<Book> foundBooks) {
+        Collections.sort(foundBooks, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                Integer valueOne = ((Book) o1).getYear();
+                Integer valueTwo = ((Book) o2).getYear();
+                int sComp = valueTwo.compareTo(valueOne);
+
+                if (sComp != 0) {
+                    return sComp;
+                } else {
+                    return -1;
+                }
+            }
+        });
 
     }
 
